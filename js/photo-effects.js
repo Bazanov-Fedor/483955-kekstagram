db4ada30ef09f191f88440fc3ea94d88903f084d
Bazanov-Fedor <foremostbeacon@gmail.com>
2018-06-28 17:51:13 +0300

    adds module5-task1

--- js/photo-effects.js
+++ js/photo-effects.js
@@ -1,45 +1,111 @@
 'use strict';
 
 (function () {
-
-  var preview = document.querySelector('.img-upload__preview');
-  var effectList = document.querySelector('.effects__list');
+  var pinPosition = {
+    MIN: 0,
+    MAX: 450
+  };
 
   var filterCss = {
-    none: 'effects__preview effects__preview--none',
-    chrom: 'effects__preview effects__preview--chrome',
-    sepia: 'effects__preview effects__preview--sepia',
-    invert: 'effects__preview effects__preview--marvin',
-    blur: 'effects__preview effects__preview--phobos',
-    brightness: 'effects__preview effects__preview--heat'
+    none: {
+      class: 'effects__preview--none'
+    },
+    chrome: {
+      class: 'effects__preview--chrome',
+      max: 1,
+      min: 0
+    },
+    sepia: {
+      class: 'effects__preview--sepia',
+      max: 1,
+      min: 0
+    },
+    marvin: {
+      class: 'effects__preview--marvin',
+      max: 100,
+      min: 0,
+      postFix: '%'
+    },
+    phobos: {
+      class: 'effects__preview--phobos',
+      max: 3,
+      min: 0,
+      postFix: 'px'
+    },
+    heat: {
+      class: 'effects__preview--heat',
+      max: 3,
+      min: 1
+    }
+  };
+
+  var preview = document.querySelector('.img-upload__preview');
+  var effectValue = document.querySelector('.scale__value');
+  var line = document.querySelector('.scale__line');
+  var pin = document.querySelector('.scale__pin');
+  var blockPin = document.querySelector('.img-upload__scale');
+
+  pin.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+    var startCoords = evt.clientX;
+
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+
+      var shift = startCoords - moveEvt.clientX;
+      var position = pin.offsetLeft - shift;
+      startCoords = moveEvt.clientX;
+
+      if (position <= pinPosition.MIN) {
+        position = pinPosition.MIN;
+      }
+      if (position > pinPosition.MAX) {
+        position = pinPosition.MAX;
+      }
+
+      pin.style.left = position + 'px';
+      line.style.width = position + 'px';
+
+      var filterChange = function (max, min, filter, filterPostfix) {
+        var postFix = filterPostfix || '';
+        var value = (max - min) * (position / pinPosition.MAX) + min;
+        var change = '' + filter + '(' + value + postFix + ')';
+
+        preview.style.filter = change;
+        effectValue.value = change;
+      };
+
+      filterChange(1, 0, 'sepia');
+      // filterChange(3, 1, 'brightness');
+      // filterChange(100, 0, 'invert', '%');
+    };
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+  });
+
+  var effectList = document.querySelector('.effects__list');
+  var upload = document.querySelector('.img-upload__overlay');
+  // временно
+  upload.classList.remove('hidden');
+
+  var cheskScaleShow = function (elem) {
+    return elem.value !== 'none' ? blockPin.classList.remove('hidden') : blockPin.classList.add('hidden');
   };
 
   effectList.addEventListener('click', function (evt) {
-    var toggler = evt.target.closest('span');
+    var toggler = evt.target.closest('input');
     if (toggler) {
-      switch (toggler.className) {
-        case 'effects__preview effects__preview--none':
-          preview.className = filterCss.none;
-          break;
-        case 'effects__preview effects__preview--chrome':
-          preview.className = filterCss.chrom;
-          break;
-        case 'effects__preview effects__preview--sepia':
-          preview.className = filterCss.sepia;
-          break;
-        case 'effects__preview effects__preview--marvin':
-          preview.className = filterCss.invert;
-          break;
-        case 'effects__preview effects__preview--phobos':
-          preview.className = filterCss.blur;
-          break;
-        case 'effects__preview effects__preview--heat':
-          preview.className = filterCss.brightness;
-          break;
-
-        default:
-          break;
-      }
+      preview.classList = 'img-upload__preview';
+      preview.classList.add(filterCss[toggler.value].class);
+      cheskScaleShow(toggler);
     }
   });
 })();
---
+++ js/scale-post.js
@@ -0,0 +1,38 @@
+'use strict';
+
+(function () {
+  var preview = document.querySelector('.img-upload__preview');
+  var resizeInput = document.querySelector('.resize__control--value');
+  var btnResizeMin = document.querySelector('.resize__control--minus');
+  var btnResizePlus = document.querySelector('.resize__control--plus');
+
+  var scaleData = {
+    MAX_SIZE: 100,
+    MIN_SIZE: 25,
+    STEP: 25,
+    START_ELEM: 0,
+    END_ELEM: -1
+  };
+
+  var onClickScaleBtn = function (action) {
+    var value = +resizeInput.value.slice(scaleData.START_ELEM, scaleData.END_ELEM);
+
+    if (action === 'reduce' && value !== scaleData.MIN_SIZE) {
+      value = value - scaleData.STEP;
+    }
+
+    if (action === 'increase' && value !== scaleData.MAX_SIZE) {
+      value = value + scaleData.STEP;
+    }
+
+    preview.style.transform = 'scale(0' + value / 100 + ')';
+    resizeInput.value = value + '%';
+  };
+
+  btnResizeMin.addEventListener('click', function () {
+    onClickScaleBtn('reduce');
+  });
+  btnResizePlus.addEventListener('click', function () {
+    onClickScaleBtn('increase');
+  });
+})();
